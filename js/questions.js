543ba5ec0c22b940abea71c07d2f47264e092f7b
talitha2013 <92562789+talitha2013@users.noreply.github.com>
2021-10-18 12:29:01 -0300

    index.html, style.css, questions.js, script,js

---
+++ js/questions.js
@@ -0,0 +1,123 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "O que significa HTML?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "O que CSS significa?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "O que significa PHP?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "O que significa SQL?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "O que XML significa?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+     numb: 6,
+     question: "Qual tag é usada para modular o formulário? (Modular é fazer uma linha ao redor, como se fosse um retângulo na parte externa)",
+     answer: "fieldset",
+     options: [
+       "h1",
+       "form",
+       "tr",
+       "fieldset"
+     ]
+   },
+
+
+   {
+    numb: 7,
+    question: "Qual a versão do HTML é mais utilizada atualmente.",
+    answer: "HTML 5",
+    options: [
+      "XHTML",
+      "XHTML 2.0",
+      "HTML 4",
+      "HTML 5"
+    ]
+  },
+
+  {
+  numb: 8,
+    question: "Todos esses softwares podem programar código HTML, exceto:",
+    answer: "Notepad+",
+    options: [
+      "Notepad++",
+      "Notepad",
+      "Notepad+",
+      "VS Code"
+    ]
+  },
+
+  {
+    numb: 9,
+      question: "Identifique o elemento HTML presente somente em HTML5",
+      answer: "figure",
+      options: [
+        "figure",
+        "frame",
+        "legend",
+        "label"
+      ]
+    },
+
+  
+    {
+      numb: 10,
+        question: "Quem criou o HTML?",
+        answer: "Tim Berbers-Lee",
+        options: [
+          "Bill Gates",
+          "Ellon Musk",
+          "Mark Zuckerberg",
+          "Tim Berbers-Lee"
+        ]
+      },
+
+];
